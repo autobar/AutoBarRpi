57649ab2cfed425cf43fe7ac14750d7b02a7f997
ZihaoPTS <523710043@qq.com>
2019-04-18 12:46:59 -0500

    update off

--- off.py
+++ off.py
@@ -5,6 +5,6 @@ import RPi.GPIO as io
 io.setmode(io.BOARD)
 io.setwarnings(False)
 
-for i in [11,33,35,37,36,38,40]:
+for i in [29,33,35,37,36,38,40]:
     io.setup(i, io.OUT)
     io.output(i, io.LOW)
