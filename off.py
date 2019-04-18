c0f53b2d23255d46e551c3b46280ce2d63c391dc
domfabian1 <dominickfabian@gmail.com>
2019-04-18 11:58:46 -0500

    edit off

--- off.py
+++ off.py
@@ -5,6 +5,9 @@ import RPi.GPIO as io
 io.setmode(io.BOARD)
 io.setwarnings(False)
 
-for i in [11,33,35,37,36,38,40]:
+for i in [33,35,37,36,38,40]:
     io.setup(i, io.OUT)
     io.output(i, io.LOW)
+
+io.setup(13, io.OUT)
+io.output(13, io.HIGH)
